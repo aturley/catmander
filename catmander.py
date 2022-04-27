e15b2c742ec8fc943e833fda19611783afd3f3f5
Andrew Turley <aturley@acm.org>
2022-04-27 10:46:01 -0500

    Increase sequencer speed
    Each sequencer step is now 250ms.

--- catmander.py
+++ catmander.py
@@ -36,7 +36,9 @@ midi = UART(6, 31250)
 
 def draw_sequence():
     global sequence
+    global sequence_ptr
     screen.fill((100, 100, 100))
+    screen.rect(sequence_ptr * 20, 0, 20, 8 * 15, color=0, fill=1)
     for i, r in enumerate(sequence):
         for j, c in enumerate(r):
             screen.text(hex(c)[2:], j * 20, i * 15, color = (255, 0, 0))
@@ -63,7 +65,8 @@ def onBtnbPressed():
         i = random.randint(0, 7)
         j = random.randint(0, 7)
         print(i, j)
-        sequence[i][j] = random.randint(0, 127)
+        velocity = random.randint(-32, 127)
+        sequence[i][j] = max(velocity, -1)
     draw_sequence()
 
 def play_note(arg):
@@ -71,14 +74,22 @@ def play_note(arg):
     global root_note
     global note_delta
     global sequence_ptr
-    
+
+    for i, r in enumerate(sequence):
+        velocity = r[sequence_ptr]
+        if velocity > -1:
+            note = root_note + note_delta[i]
+            midi.write(noteon(note, 0))
+
+    sequence_ptr = (sequence_ptr + 1) % len(sequence[0])
+
     for i, r in enumerate(sequence):
         velocity = r[sequence_ptr]
         if velocity > -1:
             note = root_note + note_delta[i]
             midi.write(noteon(note, velocity))
 
-    sequence_ptr = (sequence_ptr + 1) % len(sequence[0])
+    draw_sequence()
 
 def on_tick(t):
     micropython.schedule(play_note, None)
@@ -90,7 +101,7 @@ sensor.startSchedule()
 
 timer = Timer(1)
 
-timer.init(period=2000, callback=on_tick)
+timer.init(period=250, callback=on_tick)
 
 screen.sync = 0
 
